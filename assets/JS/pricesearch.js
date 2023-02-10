1af1841b24d169f5e258c6e40132c7b71420c286
YemiOyedeji89 <114605895+YemiOyedeji89@users.noreply.github.com>
2023-02-10 18:25:44 +0000

    pulling buying options

--- assets/JS/pricesearch.js
+++ assets/JS/pricesearch.js
@@ -1,5 +1,7 @@
 var countryAbbr = "uk";
-var movieTitle = "avatar";
+var movieTitle = localStorage.getItem("searchInput");
+ var yearInput = localStorage.getItem("searchYear");
+ 
 
 ////FUNCTION TO SEARCH OPTIONS FOR BUY RENT OR STREAM FROM RAPID API
 function watchOptions(countryAbbr, movieTitle){
@@ -14,9 +16,52 @@ function watchOptions(countryAbbr, movieTitle){
             "X-RapidAPI-Host": "where-can-i-watch1.p.rapidapi.com"
         }
     };
-    $.ajax(settings).done(function (response) {
-        console.log(response);
+    $.ajax(settings).done(function (movieData) {
+
+        console.log(movieData);
+        
+        
+        for(let t = 0; t < movieData.length; t++){
+
+            var movieTitleFound = movieData[t].title;
+            var movieYear = movieData[t].year;
+            var options = movieData[t].options;
+            var buyOption = movieData[t].options.buy
+            
+           // console.log(options);
+            /// ADDING THE MOVIE DETAILS
+            if(movieTitle == ((movieTitleFound).toLowerCase())  && yearInput == movieYear ){
+               
+                ///MOVIE TITIE
+                $(".navbar-brand").text(movieTitleFound);
+               
+                ///CONDITION TO DISPLAY THE BUY CARD OPTION
+                console.log(buyOption);
+                for(b = 0; b < buyOption.length; b++){
+                    var provider = buyOption[b].provider;
+                    var price = buyOption[b].pricing;
+                    var url = buyOption[b].providerUrl;
+
+                    ///REMOVE DISPLAY YOUTBE DETAILS IF CONDITION IS TRUE
+                    if(provider === "YouTube"){
+                        $("#you-tube").removeAttr("hidden");
+                        $("#you-tube").append($("<p>").text())
+
+                        
+
+                    }
+                }
+                
+                
+
+            } 
+            
+           
+           
+        }
+
     });
+
 
 }
 watchOptions(countryAbbr, movieTitle);
